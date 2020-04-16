9b207ac7a725b0571ad66c3be3b399d1aa821769
codelie <62216257+codelie@users.noreply.github.com>
2020-04-16 14:06:53 -0300

    Add files via upload

--- validaData.js
+++ validaData.js
@@ -2,58 +2,83 @@ class util {
 
     static validaData(dataRecebida){
         var dataAux = dataRecebida.split("/");
-        var ano = parseInt(dataAux[2]);
-        var mes = parseInt(dataAux[1]);
-        var dia = parseInt(dataAux[0]);
+//        var ano = parseInt(dataAux[2]);
+//        var mes = parseInt(dataAux[1]);
+//        var dia = parseInt(dataAux[0]);
 
-        if ( this.eNumero(ano) && this.eNumero(mes) && this.eNumero(dia) ){
-        
-            if ( (mes<=0) || (mes>12) ){
-                //console.log('Número mês menor que 1 ou maior que 12 inválido!');
-                return false;
-            }
+        var ano = dataAux[2];
+        var mes = dataAux[1];
+        var dia = dataAux[0];
 
-            if ( (dia>31) || (dia <=0) ){
-                //console.log('Número dia maior que 31 ou menor que 1 inválido!');
-                return false;
-            }
-            
-            if ( (dia>=30) && (mes==2) ){
-                console.log('Número dia 30 ou maior para fevereiro inválido!');
-                return false;
-            }
 
-            if ( (dia==29) && (mes==2) && ( (ano % 4) != 0 ) ){
-                console.log('Número dia 29 para fevereiro não bissexto inválido!');
-                return false;
-            }
-
-            if ( (dia==31) && ( (mes==2) || (mes==4) || (mes==6) || (mes==9) || (mes==11)  ) ){
-                console.log('Número dia 31 para mês inválido!');
-                return false;
-            }
-
-            if ( (1<=mes<=12) && (1<=dia<=31) ){
-                console.log('Data Validada!');
-                return true;
-            }
-        }
-        else{ 
-            console.log('Dados não numéricos. Invalidado!');
+        if ( (ano == null) || (mes == null) || (dia == null) )
+        {
             return false;
-        }        
+        }
+
+        
+        if ( this.eNumero(ano) && this.eNumero(mes) && this.eNumero(dia) ){
+
+            if (ano>0){
+                if ( (mes<=0) || (mes>12) ){
+                    //console.log('Número mês menor que 1 ou maior que 12 inválido!');
+                    return false;
+                }
+
+                if ( (dia>31) || (dia <=0) ){
+                    //console.log('Número dia maior que 31 ou menor que 1 inválido!');
+                    return false;
+                }
+                
+                if ( (dia>=30) && (mes==2) ){
+                    console.log('Número dia 30 ou maior para fevereiro inválido!');
+                    return false;
+                }
+
+                if ( (dia==29) && (mes==2) && ( (ano % 4) != 0 ) ){
+                    console.log('Número dia 29 para fevereiro não bissexto inválido!');
+                    return false;
+                }
+
+                if ( (dia==31) && ( (mes==2) || (mes==4) || (mes==6) || (mes==9) || (mes==11)  ) ){
+                    console.log('Número dia 31 para mês inválido!');
+                    return false;
+                }
+
+                if ( (1<=mes<=12) && (1<=dia<=31) ){
+                    console.log('Data Validada!'+dia+'/'+mes+'/'+ano);
+                    return true;
+                }
+
+            }
+            else
+            {
+                console.log('Ano negativo. Invalidado!');
+                return false;            
+            }                
+
+            }
+            else{ 
+                console.log('Dados não numéricos. Invalidado!');
+                return false;
+            } 
+       
         return false;
 
     } // fim validaData
 
     static eNumero(valor){
-        return typeof valor === "number";
+        return !isNaN(valor) && isFinite(valor);
+        //return typeof valor === "number";
     }   
-
+    static eNumeroFloat(valor){
+        return !isNaN(parseFloat(valor)) && isFinite(n);
+        //return typeof valor === "number";
+    } 
 } //fim classe util
 
 //var objUtil = new util;
-if (util.validaData('32/02/2004') ){
+if (util.validaData('31/03/2004') ){
     console.log('Validado!');
     document.write('VALIDADO');
 }
